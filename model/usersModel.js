ea0677f1605b8ff7e64b158e6811301c1eec76bc
Dennis Matheuwezen <dennis.matheuwezen@student.ap.be>
2023-01-17 11:40:43 +0100

    first

---
+++ model/usersModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+const autoIncrement = require("mongoose-auto-increment");
+
+autoIncrement.initialize(mongoose);
+
+const UsersSchema = new Schema({
+    //id: Number,
+    name: String,
+    authId: String,
+    image: String
+});
+
+//UsersSchema.plugin(autoIncrement.plugin, { model: "Users", field: "id" });
+module.exports = Users = mongoose.model('Users', UsersSchema);
