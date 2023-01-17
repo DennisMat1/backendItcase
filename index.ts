de2714ceed80550c6899785415ed7f774c0ae518
Dennis Matheuwezen <dennis.matheuwezen@student.ap.be>
2023-01-17 11:54:46 +0100

    first

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-var cors = require('cors')
+var cors = require("cors");
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://SumayaSha:Ben12345@clustersasu.98kvr.mongodb.net/ITCase?retryWrites=true&w=majority');
+mongoose.connect(
+  "mongodb+srv://SumayaSha:Ben12345@clustersasu.98kvr.mongodb.net/ITCase?retryWrites=true&w=majority"
+);
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+// test
 
 const QuizRoute = require("./api/Quiz");
 const UserRoute = require("./api/User");
@@ -29,5 +32,4 @@ app.listen(port, () => {
   console.log(`Ventigrate Backend Loading on: ${port}`);
 });
 
-export { }
-
+export {};
